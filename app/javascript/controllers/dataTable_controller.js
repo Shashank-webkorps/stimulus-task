008ad7c51b137692094a4d9c1ff048cdb0870a4e
shashank-webkorps <shashank@webkorps.com>
2022-05-02 23:55:21 +0530

    added css

--- app/javascript/controllers/dataTable_controller.js
+++ app/javascript/controllers/dataTable_controller.js
@@ -5,17 +5,17 @@ export default class extends Controller {
   initialize() {
     window.jQuery(this.element).dataTable({
       searching: true,
-      "ajax": {
-        "url": $('#users-datatable').data('source'),
+      'ajax': {
+        'url': $('#users-datatable').data('source'),
         dataSrc: ''
       },
       columns: [
        {data: 'teacher'},{data: 'school'},{data: 'school_year'}
       ],
-      "columnDefs": [
-        {"className": "text-center", "targets": "_all"}
+      'columnDefs': [
+        {'className': 'text-center', 'targets': '_all'}
       ],
-      "lengthMenu": [ 25, 50, 75, 100 ]
+      'lengthMenu': [ 25, 50, 75, 100 ]
     });
   }
 }
